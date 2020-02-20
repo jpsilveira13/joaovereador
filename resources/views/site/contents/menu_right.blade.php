2964dfac8f5c6aee4a3a6c246925480d3e6bc995
João Paulo da SIlveira <samotinho@gmail.com>
2020-02-20 17:30:41 -0300

    Começo site vereador

---
+++ resources/views/site/contents/menu_right.blade.php
@@ -0,0 +1,25 @@
+<section id="header">
+    <header>
+        <span class="image avatar"><img src="{{url('site/images/joaovereador.jpg')}}" alt="" /></span>
+        <h1 id="logo"><a href="#">João Paulo da Silveira</a></h1>
+        <p>Eu quero uma Uberaba melhor para todos<br />
+            e isso não é uma loucura.</p>
+    </header>
+    <nav id="nav">
+        <ul>
+            <li><a href="#one" class="active">Sobre</a></li>
+            <li><a href="#two">Projetos</a></li>
+            <li><a href="#three">Possibilidades</a></li>
+            <li><a href="#four">Contato</a></li>
+        </ul>
+    </nav>
+    <footer>
+        <ul class="icons">
+            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+
+            <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+    </footer>
+</section>
---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<!--
+	Read Only by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Nova Uberaba | João Paulo Silveira </title>
+    <meta name="description" content="Uma pessoa jovem que sonha em ajudar a construir uma nova Uberaba"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="{{asset('site/css/main.css')}}" />
+</head>
+<body class="is-preload">
+
+<!-- Header -->
+@yield('header')
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Main -->
+@yield('content')
+
+<!-- Footer -->
+
+    @yield('footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{asset('site/js/jquery.min.js')}}"></script>
+<script src="{{asset('site/js/jquery.scrollex.min.js')}}"></script>
+<script src="{{asset('site/js/jquery.scrolly.min.js')}}"></script>
+<script src="{{asset('site/js/browser.min.js')}}"></script>
+<script src="{{asset('site/js/breakpoints.min.js')}}"></script>
+<script src="{{asset('site/js/util.js')}}"></script>
+<script src="{{asset('site/js/main.js')}}"></script>
+
+</body>
+</html>
